485f596e3f9803c893e6642d7fca58825d49abb5
Scott Olesen <ulp7@cdc.gov>
2024-11-08 12:18:11 -0500

    working

--- nisapi/__init__.py
+++ nisapi/__init__.py
@@ -16,7 +16,7 @@ def get_dataset_cache_path(id: str, cache_path: Path = None) -> Path:
     return Path(cache_path) / f"id={id}" / "part-0.parquet"
 
 
-def cache_dataset(id: str, app_token=None, **kwargs) -> None:
+def cache_dataset(id: str, app_token=None) -> None:
     path = get_dataset_cache_path(id)
 
     # ensure that there is a directory to save the data to
@@ -27,14 +27,12 @@ def cache_dataset(id: str, app_token=None, **kwargs) -> None:
         data_dir.mkdir()
 
     with Socrata("data.cdc.gov", app_token) as client:
-        df = client.get(id, **kwargs)
+        df = client.get(id)
 
     pl.DataFrame(df).write_parquet(path)
 
 
-def get_dataset(
-    id: str, app_token=None, cache_path: Path = None, **kwargs
-) -> pl.DataFrame:
+def get_dataset(id: str, app_token=None, cache_path: Path = None) -> pl.DataFrame:
     if cache_path is None:
         cache_path = default_cache_path()
 
@@ -43,13 +41,16 @@ def get_dataset(
         pl.scan_parquet(cache_path).select(pl.col("id").unique()).collect()["id"]
     )
 
+    if id not in cached_ids:
+        cache_dataset(id, app_token=app_token)
+
 
 def get_dataset_ids(vaccine: str, start_date: date, end_date: date) -> [str]:
     return "sw5n-wg2p"  # flu 2021/2022 through 2024/2025
     # "udsf-9v7b" # COVID-19 updated vaccine
 
 
-def get_nis(vaccine: str, start_date: date, end_date: date, app_token=None, **kwargs):
+def get_nis(vaccine: str, start_date: date, end_date: date, app_token=None):
     dataset_ids = get_dataset_ids(
         vaccine=vaccine, start_date=start_date, end_date=end_date
     )
