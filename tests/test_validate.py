fabfe310a72d5a73b1e79ba9036d919b299742f1
Scott Olesen <ulp7@cdc.gov>
2024-11-14 15:45:31 -0500

    clean up tests

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,21 +1,19 @@
-import nisapi
+from nisapi.clean import valid_age_groups
 import polars as pl
 
 
 def test_validate_age_groups():
-    assert nisapi.valid_age_groups(pl.Series(["18-49 years"]))
+    assert valid_age_groups(pl.Series(["18-49 years"]))
 
-    assert nisapi.valid_age_groups(pl.Series(["65+ years"]))
+    assert valid_age_groups(pl.Series(["65+ years"]))
 
-    assert nisapi.valid_age_groups(
-        pl.Series(["18-49 years", "50-64 years", "65+ years"])
-    )
+    assert valid_age_groups(pl.Series(["18-49 years", "50-64 years", "65+ years"]))
 
     # en dash should fail
-    assert not nisapi.valid_age_groups(pl.Series(["18–49 years"]))
+    assert not valid_age_groups(pl.Series(["18–49 years"]))
 
     # missing "years" should fail
-    assert not nisapi.valid_age_groups(pl.Series(["18-49"]))
+    assert not valid_age_groups(pl.Series(["18-49"]))
 
     # fail if there are spaces
-    assert not nisapi.valid_age_groups(pl.Series(["18 - 49 years"]))
+    assert not valid_age_groups(pl.Series(["18 - 49 years"]))
