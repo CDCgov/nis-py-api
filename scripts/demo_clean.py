e234748df901658b59d9e50c428f11c11f1764d3
Scott Olesen <ulp7@cdc.gov>
2024-11-23 13:21:46 -0500

    update cleaning demo

--- scripts/demo_clean.py
+++ scripts/demo_clean.py
@@ -6,17 +6,27 @@ import nisapi.clean.udsf_9v7b
 import altair as alt
 
 dataset_id = "udsf-9v7b"
+clean_func = nisapi.clean.udsf_9v7b.clean
+clean_tmp_path = "scripts/tmp_clean.parquet"
 
 with open("scripts/secrets.yaml") as f:
     app_token = yaml.safe_load(f)["app_token"]
 
 raw = nisapi._get_nis_raw(id=dataset_id, app_token=app_token)
 
+# show the first few rows of the raw data
 raw.head().collect().glimpse()
 
-clean = nisapi.clean.udsf_9v7b.clean(raw)
+# try to clean the data
+clean = clean_func(raw)
+
+# look at the first few rows of the partially cleaned data
 clean.head(10).collect().glimpse()
 
+# save a copy of the partially cleaned data
+clean.collect().write_parquet(clean_tmp_path)
+
+# this will fail until the dataset cleaning is complete
 Validate(id=dataset_id, df=clean)
 
 alt.Chart(
