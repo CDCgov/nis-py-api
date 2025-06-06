3f86596d0595007cd338a60d5f7cf8dab3279bf0
afmagee42 <rzg0@cdc.gov>
2025-06-06 10:04:54 -0700

    tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -4,12 +4,12 @@ import nisapi
 
 
 def test_default_cache_path():
-    path = nisapi._root_cache_path()
+    path = nisapi.root_cache_path()
     assert isinstance(path, Path)
 
 
 def test_dataset_cache_path():
-    path = nisapi._dataset_cache_path(
+    path = nisapi.dataset_cache_path(
         root_path=Path("fake_root"), type_="raw", id="1234"
     )
     assert path == Path("fake_root", "raw", "id=1234")
