5a0ee6052b2f368c94758db0dc977317c0ec9635
Edward Schrom <tec0@cdc.gov>
2025-07-24 08:47:25 -0400

    Removed extra age column mentions

--- nisapi/clean/si7g_c2bs.py
+++ nisapi/clean/si7g_c2bs.py
@@ -1,5 +1,39 @@
 import polars as pl
 
+from nisapi.clean.helpers import (
+    clean_domain,
+    clean_domain_type,
+    clean_estimate,
+    clean_geography,
+    clean_geography_type,
+    clean_indicator,
+    clean_indicator_type,
+    clean_lci_uci,
+    clean_sample_size,
+    clean_time_start_end,
+    clean_time_type,
+    clean_vaccine,
+    drop_bad_rows,
+    enforce_schema,
+    remove_duplicates,
+)
+
 
 def clean(df: pl.LazyFrame) -> pl.LazyFrame:
-    return df
+    return (
+        df.pipe(drop_bad_rows, "suppression_flag")
+        .pipe(clean_geography_type, "geography_type")
+        .pipe(clean_geography, "geography")
+        .pipe(clean_domain_type, "group_name")
+        .pipe(clean_domain, "group_category")
+        .pipe(clean_indicator_type, "indicator_label")
+        .pipe(clean_indicator, "indicator_category_label")
+        .pipe(clean_vaccine, "vaccine")
+        .pipe(clean_time_type, None, "week")
+        .pipe(clean_time_start_end, "week_ending")
+        .pipe(clean_estimate, "estimate")
+        .pipe(clean_lci_uci, "ci_half_width_95pct", "half")
+        .pipe(clean_sample_size, "unweighted_sample_size")
+        .pipe(remove_duplicates)
+        .pipe(enforce_schema)
+    )
